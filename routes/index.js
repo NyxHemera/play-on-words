6f9b0dc2a907a59e4dc76f0505811fd4c33d4cc8
TanLui <effishing@gmail.com>
2016-05-16 21:28:26 -0400

    move routes codes to users.js

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var User = require('../models/users')
+
 
 // Home Page
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express', message: req.flash() });
-});
-
-//users/:id/edit profile chg userprof.ejs
-
-router.get('/users/:id/edit', function(req, res, next) {
-  User.findById(req.params.id)
-  .then(function(user) {
-    if (!user) return next(makeError(res, 'Document not found', 404));
-      //res.send("hi iam here");
-      console.log('1');
-    res.render('edituser', { user: user });
-  }, function(err) {
-    return next(err);
-  });
-});
-
-// UPDATE
-router.put('/users/:id', function(req, res, next) {
-  User.findById(req.params.id) //find again..as its stateless like rails
-  .then(function(user) {
-    if (!user) return next(makeError(res, 'Document not found', 404));
-    // user.local.email = req.body.local.email;
-    // user.local.password= req.body.local.password;
-    user.first_name = req.body.first_name;
-    user.last_Name=req.body.last_Name;
-    user.twitter = req.body.twitter;
-    console.log(user);
-    return user.save(); //merge w data in db
-  })
-  .then(function(saved) {
-    res.redirect('/users'); //redirect to index routes
-  }, function(err) {
-    return next(err);
-  });
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../models/users');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('<h1>USERS PAGE2</h1>');
 });
+
+//users/:id/edit profile chg userprof.ejs
+
+router.get('/:id/edit', function(req, res, next) {
+  User.findById(req.params.id)
+  .then(function(user) {
+    if (!user) return next(makeError(res, 'Document not found', 404));
+      //res.send("hi iam here");
+      console.log('1');
+    res.render('edituser', { user: user });
+  }, function(err) {
+    return next(err);
+  });
+});
+
+// UPDATE
+router.put('/:id', function(req, res, next) {
+  User.findById(req.params.id) //find again..as its stateless like rails
+  .then(function(user) {
+    if (!user) return next(makeError(res, 'Document not found', 404));
+    // user.local.email = req.body.local.email;
+    // user.local.password= req.body.local.password;
+    user.first_name = req.body.first_name;
+    user.last_Name=req.body.last_Name;
+    user.twitter = req.body.twitter;
+    console.log(user);
+    return user.save(); //merge w data in db
+  })
+  .then(function(saved) {
+    res.redirect('/users'); //redirect to index routes
+  }, function(err) {
+    return next(err);
+  });
+});
+
+
 
 module.exports = router;
