5fec92dd99adb61269ab1e863b75164e3ca7ad0a
TanLui <effishing@gmail.com>
2016-05-16 21:01:28 -0400

    route put user profile

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,6 @@ router.get('/', function(req, res, next) {
 });
 
 //users/:id/edit profile chg userprof.ejs
-//????
-// router.get('/users/:id/edit', function(req,res,next){
-// 	// res.render('edituser.ejs', { message: req.flash() });
-// 	res.render('edituser.ejs',{local:email,local:password});
-// });
 
 router.get('/users/:id/edit', function(req, res, next) {
   User.findById(req.params.id)
@@ -26,6 +21,27 @@ router.get('/users/:id/edit', function(req, res, next) {
     return next(err);
   });
 });
+
+// UPDATE
+router.put('/users/:id', function(req, res, next) {
+  User.findById(req.params.id) //find again..as its stateless like rails
+  .then(function(user) {
+    if (!user) return next(makeError(res, 'Document not found', 404));
+    // user.local.email = req.body.local.email;
+    // user.local.password= req.body.local.password;
+    user.first_name = req.body.first_name;
+    user.last_Name=req.body.last_Name;
+    user.twitter = req.body.twitter;
+    console.log(user);
+    return user.save(); //merge w data in db
+  })
+  .then(function(saved) {
+    res.redirect('/users'); //redirect to index routes
+  }, function(err) {
+    return next(err);
+  });
+});
+
 
 // Signup Page
 router.route('/signup')
