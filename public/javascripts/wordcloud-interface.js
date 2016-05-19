66fa88ad58da8680f3889ac608c38e828d0c972d
TanLui <effishing@gmail.com>
2016-05-19 17:48:20 -0400

    Home page layout final

--- public/javascripts/wordcloud-interface.js
+++ public/javascripts/wordcloud-interface.js
@@ -112,7 +112,7 @@ $(document).ready(function() {
 			generateWC(getWCArr($('#wc-text-input').val()));
 		}else {
 			//generateWC(getWCArr($('#cloud-about').text()));
-			generateWC(getWCArr('Whats a word cloud? An attractive arrangement of randomly positioned words, where the more frequent words are bigger than the others.  It is also a good wayto see what words are referenced more than others. What are they for Mostly for fun! ...and now you can try it out yourself by signing up. Login to see all the word cloud that others built too.'));
+			generateWC(getWCArr('What is a word cloud? It is an attractive arrangement of randomly positioned words, where the more frequent words are bigger than the others.  It is also a good wayto see what words are referenced more than others. What are they for Mostly for fun! ...and now you can try it out yourself by signing up. Login to see all the word cloud that others built too. wdi wdi wdi wdi wdi wdi wdi wdi wdi wdi  awesome awesome awesome awesome awesome awesome awesome awesome awesome awesome awesome'));
 		}
 	}
 
